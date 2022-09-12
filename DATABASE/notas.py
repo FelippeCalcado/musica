3f287596e1576b3e54f03b294b7c51ebe0fd60e9
FelippeCalcado <98046356+FelippeCalcado@users.noreply.github.com>
2022-09-12 18:12:41 +0100

    Criacao do database close #1

---
+++ DATABASE/notas.py
@@ -0,0 +1,40 @@
+NOTAS_BS = [   'C', 'C#',
+         'Db', 'D', 'D#',
+         'Eb', 'E',
+               'F', 'F#',
+         'Gb', 'G', 'G#',
+         'Ab', 'A', 'A#',
+         'Bb', 'B'
+               ]
+
+NOTAS_S = ['C', 'C#',
+           'D', 'D#',
+           'E',
+           'F', 'F#',
+           'G', 'G#',
+           'A', 'A#',
+           'B']
+
+NOTAS_B = [      'C',
+           'Db', 'D',
+           'Eb', 'E',
+                 'F',
+           'Gb', 'G',
+           'Ab', 'A',
+           'Bb', 'B']
+
+NOTAS = {'s': NOTAS_S, 'b': NOTAS_B}
+
+GRAUS = [
+    'I', 'I#',
+    'II', 'II#',
+    'III',
+    'IV', 'IV#',
+    'V', 'V#',
+    'VI',
+    'VI#',
+    'VII'
+]
+
+
+
